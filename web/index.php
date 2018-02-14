c81e5337f6cb5c59f90be0cbb09b9a8131338797
denischerk <35740829+denischerk@users.noreply.github.com>
2018-02-14 20:03:25 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -8,13 +8,21 @@
 </head>
 <body>
 Hello
+  
+  
   <?php if (isset($_GET['submitted'])) { ?>
     <h1>The server had accepted:</h1>
     <p>radio: <?php echo htmlspecialchars($_GET['radio']); ?></p>
+  <p >text: <span style="font-weight: bold;"> <?php echo htmlspecialchars($_GET['text']); ?></span></p>
+    <p>area: <?php echo htmlspecialchars($_GET['area']); ?></p>
+    <p>chek: <?php echo htmlspecialchars($_GET['chek']); ?></p>
+     <p>sel: <?php echo htmlspecialchars($_GET['sel']); ?></p>
+      <p>color: <?php echo htmlspecialchars($_GET['color']); ?> <span style="background-color: <?php echo htmlspecialchars($_GET['radio']); ?>; ">&nbsp &nbsp &nbsp &nbsp</span></p>
     <p>Try again? <a href="/index.php">Yes!</a></p>
   <?php } ?>
 
-  <?php if (!isset($_GET['submitted'])) { ?>
+  
+  
   
   <h1>Play with the HTML form</h1>
   <form action="/index.php">
@@ -25,6 +33,24 @@ Hello
       <label for="my-radio-no">radio No: </label>
       <input id="my-radio-no" name="radio" type="radio" required value="No"></p>
     <br>
+    <label for="my-text">text: </label>         <input id="my-text" type="text" name="text" required maxlength="5"
+                                                      placeholder="Введите однострочный текст"><br>
+
+<br>
+
+<label for="my-textarea">text: </label><br>
+<textarea id="my-textarea" style="resize:none" name="area" placeholder="Введите многострочный текст"></textarea><br><br>
+
+<p><label for="my-checkbox">checkbox: </label> <input id="my-checkbox" type="checkbox" name="chek" checked></p>
+
+
+<p><label for="my-select">select: </label>     <select id="my-select" size="1" name="sel" required>
+                                                        <option value="nichego">---------</option>
+                                                        <option value="hello">hello</option>
+                                                        <option value="good bye" selected>good bye</option>
+                                                     </select></p>
+
+<p><label for="my-color">color: </label>       <input id="my-color" type="color" name="color"></p>
     <input type="submit" value="Submit">
     <input type="reset" value="Clear">
   </form>
